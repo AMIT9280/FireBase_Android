cde34c5d5db55569b81f35a64f5f2d2cd8660a24
Amit Thakkar <amth988358@gmail.com>
2022-09-14 19:27:50 +0530

    Update-Delete

--- app/src/main/java/com/firebase_app/DisplayActivity.java
+++ app/src/main/java/com/firebase_app/DisplayActivity.java
@@ -3,7 +3,10 @@ package com.firebase_app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -23,7 +26,8 @@ ListView listView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
-    listView = findViewById(R.id.list_view);
+        listView = findViewById(R.id.list_view);
+
         firebaseDatabase = FirebaseDatabase.getInstance("https://fir-app-64e4f-default-rtdb.firebaseio.com/");
         databaseReference = firebaseDatabase.getReference("people");
         ArrayList<PeopleModel> peopleModelArrayList = new ArrayList<>();
@@ -48,6 +52,16 @@ ListView listView;
             }
         });
 
-
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Intent i1 = new Intent(DisplayActivity.this,UpdateActivity.class);
+            i1.putExtra("KEY_Fn",peopleModelArrayList.get(i).getFn());
+            i1.putExtra("KEY_Ln",peopleModelArrayList.get(i).getLn());
+            i1.putExtra("KEY_Id",peopleModelArrayList.get(i).getId());
+            startActivity(i1);
+            finish();
+        }
+    });
     }
 }
--- app/src/main/java/com/firebase_app/UpdateActivity.java
+++ app/src/main/java/com/firebase_app/UpdateActivity.java
@@ -2,13 +2,69 @@ package com.firebase_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UpdateActivity extends AppCompatActivity {
+    EditText edtFn,edtLn;
+    Button btnUpdate,btnDelete;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        edtFn = findViewById(R.id.edt_fn);
+        edtLn = findViewById(R.id.edt_ln);
+
+        btnUpdate = findViewById(R.id.btn_up);
+        btnDelete = findViewById(R.id.btn_dis);
+        firebaseDatabase = FirebaseDatabase.getInstance("https://fir-app-64e4f-default-rtdb.firebaseio.com/");
+        databaseReference = firebaseDatabase.getReference("people");
+        Intent i = getIntent();
+        String strfn = i.getStringExtra("KEY_Fn");
+        String strln = i.getStringExtra("KEY_Ln");
+
+        final String strId = i.getStringExtra("KEY_Id");
+
+        edtFn.setText(strfn);
+        edtLn.setText(strln);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strfn = edtFn.getText().toString();
+                String strln = edtLn.getText().toString();
+
+                PeopleModel peopleModel = new PeopleModel();
+                peopleModel.setId(strId);
+                peopleModel.setFn(strfn);
+                peopleModel.setLn(strln);
+
+                databaseReference.child(strId).setValue(peopleModel);
+
+                Intent I = new Intent(UpdateActivity.this,DisplayActivity.class);
+                startActivity(I);
+                finish();
+
+            }
+        });
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                databaseReference.child(strId).removeValue();
+                Intent I = new Intent(UpdateActivity.this,DisplayActivity.class);
+                startActivity(I);
+                finish();
+            }
+        });
+
     }
 }
